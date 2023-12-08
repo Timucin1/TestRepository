f6c14a6f9a71c300603af469d9a9d19581a86972
Yalcin Turan <yalcin2005@gmail.com>
2023-12-08 19:42:33 +0000

    First commit - commiting all files to the repository

---
+++ testContract.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract SimpleStorage {
+    string private data;
+
+    function set(string calldata _data) public {
+        data = _data;
+    }
+
+    function get() public view returns (string memory) {
+        return data;
+    }
+}
