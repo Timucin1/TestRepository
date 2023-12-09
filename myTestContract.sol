501101156aa3ba2183b963ac93069efcf4b51e5b
Yalcin Turan <yalcin2005@gmail.com>
2023-12-09 19:58:22 +0000

    File updated for git experiment

--- myTestContract.sol
+++ myTestContract.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
-//This line for testing Git repositiory update function
+//This line for test file for Git repositiory update function
 contract SimpleStorage {
     string private data;
 
