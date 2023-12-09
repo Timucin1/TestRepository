da27597e15d8364031c2fa2c644c0387fc971d73
Timucin1 <yalcin2005@gmail.com>
2023-12-09 21:50:00 +0000

    Update myTestContract.sol

--- myTestContract.sol
+++ myTestContract.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 <<<<<<< HEAD
-//This line for test file for Git repositiory Testfunction
+//This line for test file shows on pull Request for Git repositiory Testfunction
 =======
 //This line for test file for Git Repositioray test update function
 >>>>>>> UpdateText
